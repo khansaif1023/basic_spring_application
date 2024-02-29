a084e5422bebe996768af4b3d0f832217fe03671
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-02-29 10:42:23 +0530

    Layring of API

--- src/main/java/com/accijob/basic_api_learning/PatientController.java
+++ src/main/java/com/accijob/basic_api_learning/PatientController.java
@@ -1,5 +1,8 @@
 package com.accijob.basic_api_learning;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,25 +11,33 @@ public class PatientController {
 
 PatientService patientService=new PatientService();
     @PostMapping("/addPatientViaReqBody")
-    public String addPatientToDbViaRequestBody(@RequestBody Patient patient) {
+    public ResponseEntity addPatientToDbViaRequestBody(@RequestBody Patient patient) {
 
         //This class will have endpoint majrly;
 
         String ans=patientService.addPatientToDb(patient);
-        return ans;
+        return new ResponseEntity(ans,HttpStatus.OK);
     }
 
     @GetMapping("/findOldestPatient")
-    public  Patient findOldestPatient(){
+    public  ResponseEntity findOldestPatient(){
         Patient ans=patientService.findOldestPatient();
-        return  ans;
+
+        if(ans==null){
+            return  new ResponseEntity("No Patient Were present in DB",HttpStatus.EXPECTATION_FAILED);
+        }
+        return  new ResponseEntity(ans,HttpStatus.OK);
     }
 
     @GetMapping("/viewPatient/{patientId}/")
 
-    public  Patient viewPatient(@PathVariable("patientId") Integer patientId){
+    public ResponseEntity viewPatient(@PathVariable("patientId") Integer patientId){
         Patient patient=patientService.getPatientInfo(patientId);
-        return  patient;
+
+        if(patient==null){
+            return new ResponseEntity("No Valid Patient Found",HttpStatus.BAD_REQUEST);
+        }
+        return  new ResponseEntity<>(patient,HttpStatus.OK);
     }
 
     }
