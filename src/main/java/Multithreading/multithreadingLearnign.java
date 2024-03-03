ee9b0eb67bacb16cd6969dd64d05d78db25c4a49
saifuddin khan <109955734+khansaif1023@users.noreply.github.com>
2024-03-03 14:35:08 +0530

    change

--- src/main/java/Multithreading/multithreadingLearnign.java
+++ src/main/java/Multithreading/multithreadingLearnign.java
@@ -1,5 +1,13 @@
 package Multithreading;
 
+class MyThread2 implements Runnable{
+    public void run(){
+        while (1>0){
+            System.out.println("This is my thread 2 running");
+        }
+
+    }
+}
 class MyThread extends Thread{
 
     public void run(){
@@ -15,7 +23,11 @@ public class multithreadingLearnign {
    Thread thread=new Thread(new MyThread());
    thread.start();
 
+   Thread thread2=new Thread(new MyThread2());
+   thread2.start();
+
    while (1>0){
+       //Thread.sleep(5000);
        System.out.println("Main thread");
    }
     }
